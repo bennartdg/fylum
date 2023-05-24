df1b4de57817b500c3b5ec71f157aeb17fb96bbb
Bennart Gunawan <bennart.dgunawan@gmail.com>
2023-05-24 11:36:33 +0700

    update index and main css

--- index.php
+++ index.php
@@ -9,14 +9,70 @@
   <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
   <link rel="stylesheet" href="bootstrap/css/main.css" />
   <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="https://kit.fontawesome.com/61f8d3e11d.js" crossorigin="anonymous"></script>
   <title>FyLum</title>
 </head>
 
 <body class="bg-main-purple">
-  
+  <main class="container-fluid vh-100 bg-nearly-white">
+
+  </main>
+
+  <footer class="">
+    <div class="container-fluid bg-dark p-0">
+      <div class="container">
+        <div class="d-flex justify-content-center p-5">
+          <a class="text-light text-decoration-none" href="">
+            <div class="d-flex align-items-center justify-content-center btn-purple btn-circle rounded-circle mx-2">
+              <i class="fa-brands fa-facebook-f"></i>
+            </div>
+          </a>
+          <a class="text-light text-decoration-none" href="">
+            <div class="d-flex align-items-center justify-content-center btn-purple btn-circle rounded-circle mx-2">
+              <i class="fa-brands fa-google"></i>
+            </div>
+          </a>
+          <a class="text-light text-decoration-none" href="">
+            <div class="d-flex align-items-center justify-content-center btn-purple btn-circle rounded-circle mx-2">
+              <i class="fa-brands fa-instagram"></i>
+            </div>
+          </a>
+          <a class="text-light text-decoration-none" href="">
+            <div class="d-flex align-items-center justify-content-center btn-purple btn-circle rounded-circle mx-2">
+              <i class="fa-brands fa-linkedin"></i>
+            </div>
+          </a>
+          <a class="text-light text-decoration-none" href="https://github.com/bennartdg/fylum" target="_blank">
+            <div class="d-flex align-items-center justify-content-center btn-purple btn-circle rounded-circle mx-2">
+              <i class="fa-brands fa-github"></i>
+            </div>
+          </a>
+        </div>
+        <div class="pb-5 d-flex justify-content-center">
+          <div class="d-flex justify-content-evenly" style="width: 400px;">
+            <a href="" class="link-light-purple">HOME</a>
+            <span>|</span>
+            <a href="" class="link-light-purple">PROFILE</a>
+            <span>|</span>
+            <a href="" class="link-light-purple">ABOUT</a>
+            <span>|</span>
+            <a href="" class="link-light-purple">CONTACT</a>
+          </div>
+        </div>
+        <div class="text-center pb-5">
+          <a href="index.php">
+            <img src="assets/images/icons/fylum_light.png" alt="" height="40px">
+          </a>
+        </div>
+      </div>
+      <div class="bg-secondary-purple text-center p-2">
+        <p class="text-uppercase fw-semibold m-0" style="font-size: small;">Pleasure in the job puts perfection in the work | Aristotle</p>
+        <p class="m-0" style="font-size: x-small;">&copy;2023 | FYLUM COMPANY | ALL RIGHT RESERVED</p>
+      </div>
+    </div>
+  </footer>
   <script src="bootstrap/js/bootstrap.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
 </html>

1952bca7680d5b6a9e28dc66e2003ef667d44be3
[Bennart <[bennart.dgunawan@gmail.com]>
2023-10-06 10:22:01 +0700

    .

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (isset($_SESSION['logged_in'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="assets/images/icons/fy_main.png">
+  <link rel="icon" href="assets/images/icons/fy_main.ico">
   <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
   <link rel="stylesheet" href="bootstrap/css/main.css" />
   <link href="https://cdn.jsdelivr.net/npm/@sweetalert2/theme-dark@4/dark.css" rel="stylesheet" />
